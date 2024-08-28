313e2f6629f43ca48289eeeb1e885c1200afce9b
sandipan-das-sd <sd901656@gmail.com>
2024-08-28 16:23:19 +0530

    login ui done

--- App.js
+++ App.js
@@ -1,11 +1,49 @@
+import React, { useEffect, useCallback, useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { useFonts } from 'expo-font';
+import * as SplashScreen from 'expo-splash-screen';
+import { Entypo } from '@expo/vector-icons';
+import LinearGradient from 'react-native-linear-gradient'; // Import LinearGradient
+import LoginScreen from './App/Screen/LoginScreen/LoginScreen';
+
+SplashScreen.preventAutoHideAsync();
 
 export default function App() {
+  const [loaded, error] = useFonts({
+    'outfit': require('./assets/fonts/Outfit-Regular.ttf'),
+    'outfit-medium': require('./assets/fonts/Outfit-SemiBold.ttf'),
+    'outfit-bold': require('./assets/fonts/Outfit-Bold.ttf'),
+  });
+
+  const [appIsReady, setAppIsReady] = useState(false);
+
+  useEffect(() => {
+    if (loaded || error) {
+      SplashScreen.hideAsync();
+      setAppIsReady(true);
+    }
+  }, [loaded, error]);
+
+  const onLayoutRootView = useCallback(async () => {
+    if (appIsReady) {
+      await SplashScreen.hideAsync();
+    }
+  }, [appIsReady]);
+
+  if (!appIsReady) {
+    return null;
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View
+      colors={['#b8e1fc', '#a9d2f3', '#90bae4', '#90bff0', '#6ba8e5', '#a2daf5', '#bdf3fd']}
+      style={styles.container}
+    
+    >
+       <LoginScreen/>
+       <StatusBar style='auto'/>
+     
     </View>
   );
 }
@@ -13,8 +51,6 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    paddingTop:25
   },
 });
---
+++ App/Screen/LoginScreen/LoginScreen.jsx
@@ -0,0 +1,78 @@
+import { View, StyleSheet, Image, Text, ScrollView, TouchableOpacity } from 'react-native';
+import React from 'react';
+
+import Colors from './Utlis/Colors';
+
+export default function LoginScreen() {
+  return (
+    <ScrollView style={styles.container} contentContainerStyle={styles.contentContainer}>
+      <Image
+        source={require('../../../assets/images/logo.png')}
+        style={styles.logoImage}
+      />
+      <Image
+        source={require('../../../assets/images/car-marker.png')} 
+        style={styles.bgVideo}
+        resizeMode="cover"
+        repeat
+      />
+      <View style={{ padding: 20 }}>
+        <Text style={styles.heading}>Your Ultimate EV Charging Station Finder App</Text>
+        <Text style={styles.desc}>Find EV charging station near you, plan a trip, and so much more in just one click.</Text>
+        <TouchableOpacity style={styles.button}
+        onPress={()=>{
+            console.log("Button Cliked")
+        }}
+        >
+            <Text style={{
+                color:Colors.WHITE,
+                textAlign:'center',
+                fontFamily:'outfit',
+                fontSize:17
+
+            }}> Login with Google</Text>
+        </TouchableOpacity>
+      </View>
+    </ScrollView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    marginTop: 80,
+  },
+  contentContainer: {
+    display: 'flex',
+    alignItems: 'center',
+  },
+  logoImage: {
+    width: 200,
+    height: 100,
+    resizeMode: 'contain', 
+  },
+  bgVideo: {
+    width: '100%',
+    height: 200,
+    marginTop: 10,
+  },
+  heading: {
+    fontSize: 25,
+    fontFamily: 'outfit-bold',
+    textAlign: 'center',
+    marginTop: 20,
+  },
+  desc: {
+    fontSize: 17,
+    fontFamily: 'outfit',
+    marginTop: 15,
+    textAlign: 'center',
+    color: Colors.Gray,
+  },
+  button:{
+    backgroundColor:Colors.PRIMARY,
+    padding:16,
+    display:'flex',
+    borderRadius:99,
+    marginTop:40
+  }
+});
